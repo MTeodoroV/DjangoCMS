ff250ceb3fe526c482e153969a2cc641a32ae903
mtbaptista <mtbaptista@huia.haus>
2022-03-07 17:22:41 -0300

    Subindo o projeto

---
+++ project/mysite/polls_cms_integration/cms_toolbars.py
@@ -0,0 +1,16 @@
+from cms.toolbar_base import CMSToolbar
+from cms.toolbar_pool import toolbar_pool
+from polls.models import Poll
+
+
+class PollToolbar(CMSToolbar):
+
+    def populate(self):
+        self.toolbar.get_or_create_menu(
+            'polls_cms_integration-polls',  # a unique key for this menu
+            'Polls',                        # the text that should appear in the menu
+            )
+
+
+# register the toolbar
+toolbar_pool.register(PollToolbar)
